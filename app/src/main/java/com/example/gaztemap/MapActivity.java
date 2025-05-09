93f585d7e29f46cfd27a1f4131a328212e46c15a
ibais <ibaisologestoa71@gmail.com>
2025-05-09 23:22:16 +0200

    Añadida estructura para editar usuario

--- app/src/main/java/com/example/gaztemap/MapActivity.java
+++ app/src/main/java/com/example/gaztemap/MapActivity.java
@@ -1,8 +1,13 @@
 package com.example.gaztemap;
 
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -46,7 +51,17 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
             txtEmail.setText("usuario@ejemplo.com");
             // imagen: imgPerfil.setImageResource(fotolukenserver);
         }
+        Button botonEdit = headerView.findViewById(R.id.btnEditUser);
+        botonEdit.setOnClickListener(v -> {
+            View dialogView = getLayoutInflater().inflate(R.layout.dialog_edit, null);
 
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setView(dialogView);
+
+            AlertDialog dialog = builder.create();
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
+        });
         // botón para abrir el menú lateral
         FloatingActionButton menuButton = findViewById(R.id.menu_button);
         menuButton.setOnClickListener(view -> {
@@ -97,6 +112,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
         } else if (id == R.id.amigos) {
         } else if (id == R.id.top500) {
         } else if (id == R.id.opciones) {
+
         }
 
         drawerLayout.closeDrawer(GravityCompat.START);
