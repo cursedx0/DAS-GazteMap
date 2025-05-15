6773341b0afa60228d3ba614d21d942985e2438b
cursed <cursedxo@proton.me>
2025-05-16 01:49:45 +0200

    Checkpoint buscador de personas y solicitudes. El buscador encuentra las personas, falta funcionalidad de botón añadir (también que se deshabilite cuando ya se haya enviado la solicitud o ya se tenga amistad) y fragment para ver las solicitudes pendientes del usuario.

---
+++ app/src/main/java/com/das/gaztemap/BuscarAmigosFragment.java
@@ -0,0 +1,107 @@
+package com.das.gaztemap;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuscarAmigosFragment extends Fragment {
+
+    private EditText buscador;
+    private RecyclerView recyclerView;
+    private PersonasAdapter adapter;
+    private List<Amigo> listaAmigos = new ArrayList<>();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_buscar_amigos, container, false);
+
+        buscador = view.findViewById(R.id.editBuscar);
+        recyclerView = view.findViewById(R.id.recyclerAmigos);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new PersonasAdapter(listaAmigos, requireContext());
+        recyclerView.setAdapter(adapter);
+
+        buscador.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+                buscarAmigos(s.toString());
+            }
+            @Override public void afterTextChanged(Editable s) {}
+        });
+
+        /*
+        Button buttonAtras = view.findViewById(R.id.buttonAtrasBAF);
+        buttonAtras.setOnClickListener(v -> {
+            finish();
+        });
+        */
+        return view;
+    }
+
+    private void buscarAmigos(String query) {
+        listaAmigos.clear(); //aunque no sean amigos, sino personas
+        if (!query.isEmpty()) {
+            Data inputData = new Data.Builder()
+                    .putString("accion", "buscar_personas")
+                    .putString("url","1")
+                    .putString("usuario", query)
+                    .build();
+
+            OneTimeWorkRequest request = new OneTimeWorkRequest.Builder(BDConnector.class)
+                    .setInputData(inputData)
+                    .build();
+
+            WorkManager.getInstance(requireContext()).enqueue(request);
+            WorkManager.getInstance(requireContext()).getWorkInfoByIdLiveData(request.getId())
+                    .observe(getViewLifecycleOwner(), workInfo -> {
+                        if (workInfo != null && workInfo.getState().isFinished()) {
+                            String json = workInfo.getOutputData().getString("personas");
+                            // Parsear JSON a lista de amigos y actualizar el adaptador
+                            listaAmigos.clear();
+                            listaAmigos.addAll(parsearJson(json));
+                            Log.d("AMIGOS",listaAmigos.toString());
+                            adapter.notifyDataSetChanged();
+                            if(listaAmigos.isEmpty()){
+                                Toast.makeText(requireContext(), getString(R.string.sinResultados), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    private List<Amigo> parsearJson(String json) {
+        // Parsea JSON a lista de objetos Amigo
+        Gson gson = new Gson();
+        TypeToken<List<Amigo>> typeToken = new TypeToken<List<Amigo>>() {}; //se reutiliza la clase amigo por conveniencia
+        return gson.fromJson(json, typeToken.getType());
+    }
+}
+
